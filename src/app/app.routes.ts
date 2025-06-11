150abe71cc7bcb822f60dc03ba8a851803bfa3b8
Safynazibrahim <safynazibrahim4@gmail.com>
2025-06-12 00:52:10 +0300

    base structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './components/core/guards/guest/guest.guard';
+import { authGuard } from './components/core/guards/auth/auth.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    // Public routes (for all users)
+  {
+    path: '',
+    loadComponent: () => import('./components/layouts/public-layout/public-layout.component').then(m => m.PublicLayoutComponent),
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { 
+        path: 'home', 
+        loadComponent: () => import('./components/public/home/home-component.component').then(m => m.HomeComponentComponent)
+      },
+      { 
+        path: 'about', 
+        loadComponent: () => import('./components/public/about/about-component.component').then(m => m.AboutComponentComponent)
+      },
+      { 
+        path: 'contact', 
+        loadComponent: () => import('./components/public/contact/contact.component').then(m => m.ContactComponent)
+      },
+      { 
+        path: 'login', 
+        loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent),
+        canActivate: [guestGuard] // Only for non-authenticated users
+      },
+      { 
+        path: 'register', 
+        loadComponent: () => import('./components/auth/register/register.component').then(m => m.RegisterComponent),
+        canActivate: [guestGuard] // Only for non-authenticated users
+      }
+    ]
+  },
+
+    // Protected routes (for authenticated users only)
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./components/layouts/user-layout/user-layout.component').then(m => m.UserLayoutComponent),
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: '/dashboard/home', pathMatch: 'full' },
+      { 
+        path: 'home', 
+        loadComponent: () => import('./components/user/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      { 
+        path: 'profile', 
+        loadComponent: () => import('./components/user/profile/profile.component').then(m => m.ProfileComponent)
+      }
+    ]
+  },
+
+  // Wildcard route
+  { path: '**', redirectTo: '/home' }
+
+];
---
+++ src/app/components/auth/forget-pass/forget-pass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-pass',
+  standalone: true,
+  imports: [],
+  templateUrl: './forget-pass.component.html',
+  styleUrl: './forget-pass.component.scss'
+})
+export class ForgetPassComponent {
+
+}
