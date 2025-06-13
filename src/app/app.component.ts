532915f9f1b4c032cbfb083bc6406828beb04e33
Donia <doniia.aliiii2@gmail.com>
2025-06-13 04:25:19 +0300

    navbar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponentComponent } from './components/public/home/home-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HomeComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/public/home/home-component.component.ts
+++ src/app/components/public/home/home-component.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
   templateUrl: './home-component.component.html',
-  styleUrl: './home-component.component.scss'
+  styleUrls: ['./home-component.component.scss']
 })
 export class HomeComponentComponent {
+  constructor(private router: Router) {}
 
+  activeSection: string = 'home';
+  manualScrolling: boolean = false;
+
+  scrollToSection(id: string) {
+    this.manualScrolling = true;
+    this.activeSection = id;
+
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+    setTimeout(() => {
+      this.manualScrolling = false;
+    }, 800);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (this.manualScrolling) return; 
+
+    const sections = ['home', 'about', 'contact'];
+    for (let section of sections) {
+      const el = document.getElementById(section);
+      if (el) {
+        const rect = el.getBoundingClientRect();
+        if (rect.top <= 80 && rect.bottom >= 80) {
+          this.activeSection = section;
+          break;
+        }
+      }
+    }
+  }
 }
