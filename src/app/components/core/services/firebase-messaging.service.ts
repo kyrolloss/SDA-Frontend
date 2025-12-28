1dd5aaff4e23910fcc7439ddd088e568e198ebc7
Safynazibrahim <safynazibrahim4@gmail.com>
2025-12-28 23:43:59 +0200

    firebase setup is done

--- src/app/components/core/services/firebase-messaging.service.ts
+++ src/app/components/core/services/firebase-messaging.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Messaging, getToken, onMessage } from '@angular/fire/messaging';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseMessagingService {
-
   constructor(private messaging: Messaging) {}
+
+  
 
   requestPermission() {
     return getToken(this.messaging, {
-      vapidKey: "YOUR_VAPID_KEY"
-    }).then(token => {
-      console.log("FCM Token:", token);
-      return token;
-    });
+      vapidKey: environment.vapidKey,
+    })
+      .then((token) => {
+        if (!token) {
+          console.warn('No FCM token generated');
+          return null;
+        }
+        return token;
+      })
+      .catch((err) => {
+        console.error('FCM error:', err);
+        return null;
+      });
   }
 
   listen() {
-    onMessage(this.messaging, payload => {
-      console.log("Message received:", payload);
+    onMessage(this.messaging, (payload) => {
+      console.log('Message received:', payload);
     });
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,16 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api', // your local backend during development
-    /// fake data
-    firebaseConfig: {
-    apiKey: "xxxx",
-    authDomain: "xxxx.firebaseapp.com",
-    projectId: "xxxx",
-    storageBucket: "xxxx.appspot.com",
-    messagingSenderId: "xxxx",
-    appId: "xxxx",
-    measurementId: "xxxx"
-    }
+  production: false,
+  apiUrl: 'http://localhost:3000/api', // your local backend during development
+  firebaseConfig: {
+    apiKey: 'AIzaSyDMWzVkMlmgNft4pKNvCpuC_y383M21mRk',
+    authDomain: 'one-ai-5f96b.firebaseapp.com',
+    projectId: 'one-ai-5f96b',
+    storageBucket: 'one-ai-5f96b.firebasestorage.app',
+    messagingSenderId: '61536759010',
+    appId: '1:61536759010:web:f3bc064fcf5ef2930e42bb',
+    measurementId: 'G-FPEQJC3V9T',
+  },
+
+  vapidKey:
+    'BBFeb0YagBNutvnZpiCRxdAndRRl74UKxgpN4iSz-j1ThiArpiFmnSUCP6le5ar1cvvqBN0LLW93JqcgeXlGPE4',
 };
