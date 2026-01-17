7158c13acba0603a4ef5ae160b1779772b921294
Safynazibrahim <safynazibrahim4@gmail.com>
2026-01-17 16:55:39 +0200

    labs in progress

--- src/app/components/user/laboratory/labs-system/tabs/expences/expences.component.ts
+++ src/app/components/user/laboratory/labs-system/tabs/expences/expences.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { LabService } from '../../../lab.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { PaginationComponent } from '../../../../../shared/pagination/pagination.component';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../../../../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-expences',
   standalone: true,
-  imports: [MatIcon, TranslateModule],
+  imports: [MatIcon, TranslateModule, PaginationComponent, CommonModule, ModalComponent],
   templateUrl: './expences.component.html',
   styleUrl: './expences.component.scss',
 })
@@ -14,13 +17,28 @@ export class ExpencesComponent implements OnInit {
   // =====================
   // DATA
   // =====================
-  total = signal(0);
+  totalExpenses = signal(0);
   loading = signal(false);
+  orders = signal<any[]>([]);
+  totalOrders = signal(0);
+
+  // =====================
+  // PAGINATION
+  // =====================
+  page = signal(1);
+  limit = signal(6);
+
+  // =====================
+  // View Modal
+  // =====================
+  isOpenViewOrder = false;
+  selectedOrder:any;
 
   constructor(private _LabService: LabService) {}
 
   ngOnInit(): void {
     this.loadExpenses();
+    this.loadOrders();
   }
 
   loadExpenses(): void {
@@ -29,8 +47,63 @@ export class ExpencesComponent implements OnInit {
     this._LabService
       .getExpenses()
       .then((res) => {
-        this.total.set(res.total);
+        this.totalExpenses.set(res.total);
       })
       .finally(() => this.loading.set(false));
+  }
+
+  loadOrders(): void {
+    this.loading.set(true);
+
+    const params: any = {
+      page: this.page(),
+      limit: this.limit(),
+      status : 'completed'
+    };
+
+    this._LabService
+      .getOrders(params)
+      .then((res) => {
+        this.orders.set(res.data);
+        this.totalOrders.set(res.total);
+      })
+      .finally(() => this.loading.set(false));
+  }
+
+  onPageChange(page: number): void {
+    this.page.set(page);
+    this.loadOrders();
+  }
+
+  getStatusIcon(status: string): string {
+    switch (status) {
+      case 'waiting':
+        return 'schedule';
+      case 'in_progress':
+        return 'autorenew';
+      case 'try_in':
+        return 'visibility';
+      case 'completed':
+        return 'check_circle';
+      default:
+        return 'help_outline';
+    }
+  }
+
+  getOrderDetails(orderId: number) {
+    this._LabService
+      .getOrderDetails(orderId)
+      .then((res) => {
+        this.selectedOrder = res;
+      })
+      .finally(() => this.loading.set(false));
+  }
+
+  openViewModal(orderId: number) {
+    this.isOpenViewOrder = true;
+    this.getOrderDetails(orderId);
+  }
+  closeViewModal() {
+    this.isOpenViewOrder = false;
   }
 }
