9311c1c2dd75ceed388cfc5788e8c3c4dc24c8dd
Safynazibrahim <safynazibrahim4@gmail.com>
2026-04-09 22:36:42 +0200

    fix: use double quotes in env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-staging-47c6.up.railway.app/api',
-  geminiApiKey: '$GEMINI_API_KEY',
+  geminiApiKey: "$GEMINI_API_KEY",
   firebaseConfig: {
-    apiKey: '$FIREBASE_API_KEY',
+    apiKey: "$FIREBASE_API_KEY",
     authDomain: 'one-ai-5f96b.firebaseapp.com',
     projectId: 'one-ai-5f96b',
     storageBucket: 'one-ai-5f96b.firebasestorage.app',
     messagingSenderId: '61536759010',
-    appId: '$FIREBASE_APP_ID',
-    measurementId: '$FIREBASE_MEASUREMENT',
+    appId: "$FIREBASE_APP_ID",
+    measurementId: "$FIREBASE_MEASUREMENT",
   },
-  vapidKey: '$VAPID_KEY',
+  vapidKey: "$VAPID_KEY",
 };
